1ee6d74537ddcedf36b242cd918947ff6257f376
Subhashis <subhashisdhara123@gmail.com>
2025-02-17 23:35:02 +0530

    add modal component for order creation and enhance bottom navigation

--- pos-frontend/src/components/shared/BottomNav.jsx
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { MdOutlineReorder, MdTableBar } from "react-icons/md";
 import { CiCircleMore } from "react-icons/ci";
 import { BiSolidDish } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
+import Modal from "./Modal";
 
 const BottomNav = () => {
-
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [guestCount, setGuestCount] = useState(0);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  const increment = () => {
+    if(guestCount >= 6) return;
+    setGuestCount((prev) => prev + 1);
+  }
+  const decrement = () =>{
+    if(guestCount <= 0) return;
+    setGuestCount((prev) => prev - 1);
+  }
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
-      <button onClick={() => navigate("/")} className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px] cursor-pointer">
+      <button
+        onClick={() => navigate("/")}
+        className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px] cursor-pointer"
+      >
         <FaHome className="inline mr-2" size={20} /> <p>Home</p>
       </button>
-      <button onClick={() => navigate("/orders")} className="flex items-center justify-center text-[#ababab] w-[200px] cursor-pointer">
+      <button
+        onClick={() => navigate("/orders")}
+        className="flex items-center justify-center text-[#ababab] w-[200px] cursor-pointer"
+      >
         <MdOutlineReorder className="inline mr-2" size={20} />
         <p>Orders</p>
       </button>
-      <button onClick={() => navigate("/tables")} className="flex items-center justify-center text-[#ababab] w-[200px] cursor-pointer">
+      <button
+        onClick={() => navigate("/tables")}
+        className="flex items-center justify-center text-[#ababab] w-[200px] cursor-pointer"
+      >
         <MdTableBar className="inline mr-2" size={20} />
         <p>Tables</p>
       </button>
@@ -26,9 +47,42 @@ const BottomNav = () => {
         <p>More</p>
       </button>
 
-      <button className="absolute bottom-6 bg-[#f6B100] text-[#f5f5f5] rounded-full p-3 items-center">
-        <BiSolidDish size={30} />
+      <button
+        onClick={openModal}
+        className="absolute bottom-6 bg-[#f6B100] text-[#f5f5f5] rounded-full p-4 items-center"
+      >
+        <BiSolidDish size={40} />
       </button>
+
+      <Modal
+        title="Create Order"
+        isOpen={isModalOpen}
+        onClose={closeModal}
+      >
+        <div>
+          <label className="block text-[#ababab] mb-2 text-sm font-medium">Customer Name</label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
+            <input type="text" name="" placeholder="Enter customer name" id="" className="bg-transparent flex-1 text-white focus:outline-none"/>
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">Customer Phone</label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
+            <input type="number" name="" placeholder="+91-9999999999" id="" className="bg-transparent flex-1 text-white focus:outline-none"/>
+          </div>
+        </div>
+        <div>
+          <label className="block mb-2 mt-3 text-sm font-medium text-[#ababab]">Guest</label>
+          <div className="flex items-center justify-between bg-[#1f1f1f] px-4 py-3 rounded-lg">
+            <button onClick={decrement} className="text-yellow-500 text-2xl">&minus;</button>
+            <span className="text-white">{guestCount} Person</span>
+            <button onClick={increment} className="text-yellow-500 text-2xl">&#43;</button>
+          </div>
+        </div>
+        <button onClick={()=> navigate('/tables')} className="bg-[#f6B100] text-[#f5f5f5] w-full rounded-lg p-3 mt-8 hover:bg-yellow-700">
+          Create Order
+        </button>
+      </Modal>
     </div>
   );
 };

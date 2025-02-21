2a7533e91d6f9480f46d6425ec3be7065dfd2c8d
Subhashis <subhashisdhara123@gmail.com>
2025-02-21 20:56:29 +0530

    add backend setup with Express and MongoDB; create app.js and database.js for server and database connection

---
+++ pos-backend/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/database');
+
+const app = express();
+
+const PORT = process.env.PORT;
+connectDB();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

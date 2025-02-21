1ce5064d0fb30a89bbc1848e71772cf205b538db
Subhashis <subhashisdhara123@gmail.com>
2025-02-21 21:53:19 +0530

    add configuration and error handling; set up global error handler and connect to MongoDB

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/database');
+const config = require('./config/config');
+const globalErrorHandler = require('./middleware/globalErrorHandler');
+const createHttpError = require('http-errors');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = config.port;
 connectDB();
 
+// Root EndPoint
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
